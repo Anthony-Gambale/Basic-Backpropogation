35a8c323731c47bfa727a10314a94c355d60fbee
Anthony Gambale <ants.gambale@gmail.com>
2020-12-24 08:22:42 +1100

    cleaned up

--- main.py
+++ main.py
@@ -7,9 +7,8 @@ from time import time
 
 # read the points from the csv
 data = np.genfromtxt('data.csv', delimiter=',')
-for x, y in data:
-    y *= 0.1
-    y += -100
+for point in data:
+    point[0] -= 50
 
 # init the network
 eta = 0.0003
@@ -23,7 +22,7 @@ input("Press [Enter] to begin training.")
 t_initial = time()
 
 # train the network
-testnet.train(10, data, True, 0.5)
+testnet.train(1000, data, True, 0.1)
 
 # final time
 t_final = time()
@@ -37,23 +36,15 @@ print("y = "+str(testnet.weight)+" x + "+str(testnet.bias))
 print("The finishing error is " + str(testnet.average_cost(data)))
 
 # make an x and y axis for the points
-x_list = [a[0] for a in data]
-y_list = [a[1] for a in data]
+x_list = [x for x,y in data]
+y_list = [y for x,y in data]
 
 # make an x and y axis for the line
-xa = np.linspace(0, 100, 100)
+xa = np.linspace(min(x_list), max(x_list), max(x_list)-min(x_list))
 ya = xa * testnet.weight + testnet.bias
-
-# make a black line for the y axis and a black line for the x axis to force the window to be a certain size
-xx = np.linspace(-50, 150, 100)
-yx = xa * 0
-
-yy = np.linspace(-50, 150, 100)
-xy = yy * 0
+print(xa)
 
 # plot it all
 plt.scatter(x_list, y_list, color="blue")
 plt.plot(xa, ya, color="red")
-plt.plot(xx, yx, color="black", linestyle="--")
-plt.plot(xy, yy, color="black", linestyle="--")
 plt.show()
